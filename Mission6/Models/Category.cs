3c450d77b3d1b810d93b478c8142c6ef74d02a46
abbycummings <abbycummings66@gmail.com>
2025-02-21 21:08:43 -0700

    final commit

---
+++ Mission6/Models/Category.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission6.Models;
+
+public class Category
+{
+    [Key]
+    public int CategoryId { get; set; }
+    
+    public string CategoryName { get; set; }
+    
+    public List<Movie>Movies { get; set; }
+}
